da04deafdcc615b2b2537432aaba17f00391842a
bairdlangenbrunner <blangenb@uci.edu>
2018-07-17 10:03:33 -0700

    reorganizing

--- copy_gp_FMAASO.sh
+++
@@ -1,1 +0,0 @@
-rsync -ahx --progress blangenb@gplogin2.ps.uci.edu:/beegfs/DATA/pritchard/blangenb/analysis/amazon_WRF/CHEYENNE/FMAASO/*.png .
---
+++ copy_gp_figures.sh
@@ -0,0 +1,1 @@
+rsync -ahx --progress blangenb@gplogin2.ps.uci.edu:/beegfs/DATA/pritchard/blangenb/analysis/_github_repos_research/amazon-phys-response-paper/figures .
