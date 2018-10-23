35e3c893277ce2d3573b20f987fc3caccc3feaa6
Baird Langenbrunner <langenbrunner@gmail.com>
2018-10-23 15:01:31 -0700

    first submission

--- copy_gp_figures.sh
+++ copy_gp_figures.sh
@@ -1,1 +1,1 @@
-rsync -ahx --progress blangenb@gplogin2.ps.uci.edu:/beegfs/DATA/pritchard/blangenb/analysis/_github_repos_research/amazon-phys-response-paper/figures .
+rsync -ahx --progress blangenb@gplogin2.ps.uci.edu:/beegfs/DATA/pritchard/blangenb/analysis/_github_repos_research/amazon-phys-response-paper-drafting/figures .
